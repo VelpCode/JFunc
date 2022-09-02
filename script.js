6912eb507b764a7ec0a7576118d2222f24ef46e8
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2022-09-02 14:37:52 -0400

    Button Functionality Works

--- script.js
+++ script.js
@@ -2,3 +2,7 @@
 const languages = ["Python", "Java", "JavaScript", "C hash"];
 document.getElementById("demo").innerHTML = languages;
 
+function myFunc() {
+    languages.push("Scratch");
+    document.getElementById("demo").innerHTML = languages;
+}
